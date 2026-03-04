b9401984ad2f909975473b3921e7c40cda414de1
manalBatta <manal.batta.1234@gmail.com>
2026-03-04 22:22:12 +0200

    now it should fial the tests and CI

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -1,16 +1,24 @@
 import { describe, expect, test } from "vitest";
+import { getAPIKey } from "../api/auth";
 
-const person = {
-  isActive: true,
-  age: 32,
-};
+const headers = {
+    "authorization": "ApiKey 12345"
+}
 
-describe("person", () => {
-  test("person is defined", () => {
-    expect(person).toBeDefined();
-  });
 
-  test("is active", () => {
-    expect(person.isActive).toBeTruthy();
-  });
+describe("Get API key", () => {
+    test("Fail no authorization header ", () => {
+        const result = getAPIKey({});
+        expect(result).toBeNull();
+    });
+
+    test("Fail only send one word ", () => {
+        const result = getAPIKey({ "authorization": "hello" });
+        expect(result).toBeNull();
+    });
+
+    test("Success return 12345", () => {
+        const result = getAPIKey(headers);
+        expect(result).toEqual("12345")
+    });
 });
